dcd1be44f17b0457cd50a3ed13385ea03d1ed4d8
Guillaume Berche <guillaume.berche@orange.com>
2017-10-24 12:10:25 +0200

    Initial skeletton for terraform processor

---
+++ cf-ops-automation-broker-core/src/test/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/terraform/TerraformModuleProcessorTest.java
@@ -0,0 +1,60 @@
+package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.terraform;
+
+import com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.git.GitProcessorContext;
+import com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.processors.Context;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ *
+ */
+public class TerraformModuleProcessorTest {
+
+    TerraformModuleProcessor terraformModuleProcessor = new TerraformModuleProcessor();
+
+    @Test
+    public void looks_up_paas_secrets_git_local_checkout() throws IOException {
+        //given
+        //the git mediation cloned the repo and populated context
+        Context ctx = new Context();
+        Path workDir = Files.createTempDirectory("paas-secret-clone");
+
+        ctx.contextKeys.put(GitProcessorContext.workDir.toString(),workDir);
+
+        //when
+        //Path lookedUpWorkDir = terraformModuleProcessor.lookUpGitworkDir(ctx);
+
+        //then
+        //Asserts.assertThat(lookedUpWorkDir.equals());
+
+    }
+
+    @Test
+    public void receives_service_instance_creation_inputs() {
+
+    }
+
+    @Test
+    public void validates_service_instance_inputs() {
+
+    }
+
+    @Test
+    public void check_service_instance_conflicting_state() {
+
+    }
+
+    @Test
+    public void writes_terraform_module_invocation() {
+
+    }
+
+    @Test
+    public void reads_terraform_state_for_completion() {
+
+    }
+
+}

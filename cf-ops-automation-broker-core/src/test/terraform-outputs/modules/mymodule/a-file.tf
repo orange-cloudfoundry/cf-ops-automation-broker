14b881f5d9445731cfe817bea13346a89231463c
Guillaume Berche <guillaume.berche@orange.com>
2017-11-23 08:53:04 +0100

    formatting TF specs using "terraform fmt" cmd

--- cf-ops-automation-broker-core/src/test/terraform-outputs/modules/mymodule/a-file.tf
+++ cf-ops-automation-broker-core/src/test/terraform-outputs/modules/mymodule/a-file.tf
@@ -1,19 +1,21 @@
 resource "local_file" "success_file" {
-  content     = "foo!"
+  content  = "foo!"
   filename = "/tmp/writeable/file"
 }
 
 resource "local_file" "error_file" {
-  content     = "foo!"
+  content  = "foo!"
   filename = "/tmp/non-writeable/file"
 }
 
 output "started" {
   description = "tracked module was invoked"
-  value = "successfully received module invocation"
+  value       = "successfully received module invocation"
 }
+
 output "completed" {
   description = "provides a completion status of the module to be tracked by the broker"
-  value = "successfully provisionned ${local_file.error_file.filename =="/tmp/writeable/file" ? "yes": "no"} "
+  value       = "successfully provisionned ${local_file.error_file.filename =="/tmp/writeable/file" ? "yes": "no"} "
+
   // and ${local_file.error_file.filename}
 }

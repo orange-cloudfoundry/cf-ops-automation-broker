25ed9ca70ccdb1426a1707c9ca65af7685044725
Jean-Christophe LOSAPIO <jeanchristophe.losapio@orange.com>
2017-12-14 14:03:15 +0100

    Cassandra processor

---
+++ cf-ops-automation-broker-core/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/CassandraProcessor.java
@@ -0,0 +1,59 @@
+package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.pipeline;
+
+import com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.git.GitProcessorContext;
+import com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.processors.Context;
+import com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.processors.DefaultBrokerProcessor;
+import com.orange.oss.ondemandbroker.ProcessorChainServiceInstanceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.servicebroker.model.CreateServiceInstanceRequest;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class CassandraProcessor extends DefaultBrokerProcessor {
+
+	private static Logger logger = LoggerFactory.getLogger(CassandraProcessor.class.getName());
+	private final static String ROOT_DEPLOYMENT = "coab-depls";
+
+
+	@Override
+	public void preCreate(Context ctx) {
+		//Need to retrieve workDir context
+		Path workDir = (Path) ctx.contextKeys.get(GitProcessorContext.workDir.toString());
+		logger.debug("workDir is " + workDir.toString());
+
+		//Need to retrieve service instance id from  context
+		Map<String, Object> contextKeys = ctx.contextKeys;
+		CreateServiceInstanceRequest request = (CreateServiceInstanceRequest) contextKeys.get(ProcessorChainServiceInstanceService.CREATE_SERVICE_INSTANCE_REQUEST);
+		String serviceInstanceId = request.getServiceInstanceId();
+		logger.debug("service instance id is " + serviceInstanceId);
+
+		//Check pre-requisites
+		//this.checkPrequisites(workDir);
+		
+		
+		//Generate expected Cassandra paas-template structure
+		//this.generateCassandraTemplatesStructure(workDir, serviceInstanceId);
+
+		//Generate expected Cassandra secrets structure
+		this.generateCassandraSecretsStructure(workDir, serviceInstanceId);
+
+	}
+
+	private void checkPrequisites(Path workDir) {		
+	}
+
+	private void generateCassandraSecretsStructure(Path workDir, String serviceInstanceId) {
+		File deploymentDirectory=new File(String.valueOf(workDir)+"/"+ ROOT_DEPLOYMENT +"/"+ "cassandra_" + serviceInstanceId);
+		File deploymentSecretsDirectory=new File(String.valueOf(workDir)+"/"+ ROOT_DEPLOYMENT +"/"+ "cassandra_" + serviceInstanceId+"/" + "secrets");
+		deploymentDirectory..mkdir();
+		deploymentSecretsDirectory.mkdir();
+	}
+
+	private void generateCassandraTemplatesStructure(Path workDir, String serviceInstanceId) {
+	}
+
+
+}	
+
---
+++ cf-ops-automation-broker-core/src/test/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/CassandraProcessorTest.java
@@ -0,0 +1,76 @@
+package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.pipeline;
+
+import com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.git.GitProcessorContext;
+import com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.processors.Context;
+import com.orange.oss.ondemandbroker.ProcessorChainServiceInstanceService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.servicebroker.model.CreateServiceInstanceRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CassandraProcessorTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(CassandraProcessorTest.class);
+
+//    @Rule
+//    public TemporaryFolder folder= new TemporaryFolder();
+
+
+    private final static String ROOT_DEPLOYMENT = "coab-depls";
+
+    @Test
+    public void shouldCreateDeploymentFolders() {
+
+        //Given
+        String tmpdir = System.getProperty("java.io.tmpdir");
+        System.out.println("The default value of the java.io.tmpdir system property is: \"" + tmpdir + "\"\n");
+
+        Path workDir = null;
+        try {
+            Context ctx = new Context();
+            workDir = Files.createTempDirectory("paas-secrets");
+            ctx.contextKeys.put(GitProcessorContext.workDir.toString(),workDir);
+            CreateServiceInstanceRequest request = new CreateServiceInstanceRequest("service_definition_id",
+                    "plan_id",
+                    "org_id",
+                    "space_id",
+                    null
+            );
+            request.withServiceInstanceId("guid");
+
+            //and the context being injected to a cloudflare processor
+            ctx.contextKeys.put(ProcessorChainServiceInstanceService.CREATE_SERVICE_INSTANCE_REQUEST, request);
+
+            File deploymentDirectory=new File(String.valueOf(workDir)+"/"+ ROOT_DEPLOYMENT +"/"+ "cassandra_" + "guid");
+            File deploymentSecretsDirectory=new File(String.valueOf(workDir)+"/"+ ROOT_DEPLOYMENT +"/"+ "cassandra_" + "guid" +"/" + "secrets");
+
+
+            CassandraProcessor cassandraProcessor = new CassandraProcessor();
+
+            //When
+            cassandraProcessor.preCreate(ctx);
+
+            //Then
+            assertTrue(deploymentDirectory.exists());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}

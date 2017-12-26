72444eba185515713a49d3a849ffc7a87c3fe6dd
Guillaume Berche <guillaume.berche@orange.com>
2017-12-26 11:15:07 +0100

    Bumped spring-cloud minor version & imported OSB feign client from sec-group-broker-filter
    Project at https://github.com/orange-cloudfoundry/sec-group-broker-filter
    Bumped spring-cloud to latest released GA version (edgware) and used recommended syntax at https://projects.spring.io/spring-cloud/
    Moved from spring-cloud-starter-feign to spring-cloud-starter-openfeign (see https://github.com/spring-cloud/spring-cloud-netflix/issues/2220 and https://github.com/spring-cloud/spring-cloud-netflix/blob/b9a479607c7d1dcfbb94f9cb1df951f95e08ab1e/spring-cloud-starter-feign/pom.xml#L13)
    Bumped to latest okhttp and feign-okhttp versions

---
+++ cf-ops-automation-broker-core/src/test/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/osbclient/BrokerFilterSettings.java
@@ -0,0 +1,73 @@
+/*
+ * <!--
+ *
+ *     Copyright (C) 2015 Orange
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ * -->
+ */
+
+package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.osbclient;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+
+/**
+ * Filtered broker web client connection settings
+ *
+ * @author Sebastien Bortolussi
+ */
+@Component
+@ConfigurationProperties(BrokerFilterSettings.PREFIX)
+public class BrokerFilterSettings {
+
+    public static final String PREFIX = "broker.filter";
+    private URL url;
+    private String user;
+    private String password;
+
+    public BrokerFilterSettings() {
+    }
+
+    public BrokerFilterSettings(URL url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+

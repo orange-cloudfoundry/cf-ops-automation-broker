9d914e933e1230a19f7a0eb516b2fa70c153be29
Jean-Christophe LOSAPIO <jeanchristophe.losapio@orange.com>
2018-01-02 14:40:32 +0100

    Disable OsbClientAutoConfiguration bean to make the maven compilation passed (7.2 Overriding Feign Defaults)

--- open-service-broker-client/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/OsbClientAutoConfiguration.java
+++ open-service-broker-client/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/OsbClientAutoConfiguration.java
@@ -3,9 +3,9 @@ package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages = {
-        "com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.osbclient"})
+//@Configuration
+//@ComponentScan(basePackages = {
+//        "com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.osbclient"})
 public class OsbClientAutoConfiguration {
 
 }

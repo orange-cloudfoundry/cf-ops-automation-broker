6ffb6cc5fe073cb3501263cdb010283c4d8a691a
Guillaume Berche <guillaume.berche@orange.com>
2018-05-31 16:52:11 +0200

    Rename CassandraProcessorException

--- cf-ops-automation-broker-core/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/BoshProcessorException.java
+++ cf-ops-automation-broker-core/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/BoshProcessorException.java
@@ -3,8 +3,8 @@ package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.pipeline
 /**
  * Created by ijly7474 on 14/12/17.
  */
-public class CassandraProcessorException extends RuntimeException {
-        public CassandraProcessorException(String message) {
+public class BoshProcessorException extends RuntimeException {
+        public BoshProcessorException(String message) {
             super(message);
         }
 }

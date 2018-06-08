c9aef4ffdae518d5a2758a9a945b3196cefb0136
Jean-Christophe LOSAPIO <jeanchristophe.losapio@orange.com>
2018-06-08 17:22:16 +0200

    Rename constant

--- cf-ops-automation-broker-core/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/DeploymentConstants.java
+++ cf-ops-automation-broker-core/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/DeploymentConstants.java
@@ -9,7 +9,7 @@ public class DeploymentConstants {
      * <A href="https://github.com/orange-cloudfoundry/cf-ops-automation#ops-files">COA operators</A>
      * <blockquote>By convention, all files in template dir matching *-operators.yml are used by bosh-deployment as ops-files inputs.</blockquote>
      */
-    public static final String COA_OPERATORS_FILE_PREFIX= HYPHEN + "operators" + YML_EXTENSION;
+    public static final String COA_OPERATORS_FILE_SUFFIX= HYPHEN + "operators" + YML_EXTENSION;
     public static final String DEPLOYMENT_DEPENDENCIES_FILENAME = "deployment-dependencies.yml";
     public static final String ENABLE_DEPLOYMENT_FILENAME = "enable-deployment.yml";
     public static final String DEPLOYMENT = "bosh";

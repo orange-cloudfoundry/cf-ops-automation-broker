04de2ff1f8c2c6da5b8825074af63fea09c36bb0
Jean-Christophe LOSAPIO <jeanchristophe.losapio@orange.com>
2018-06-25 17:53:42 +0200

    Integration testing utilities

---
+++ cf-ops-automation-broker-core/src/test/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/tools/Tree.java
@@ -0,0 +1,86 @@
+package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.pipeline.tools;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Tree {
+    private int dirCount;
+    private int fileCount;
+
+    public Tree() {
+        this.dirCount = 0;
+        this.fileCount = 0;
+    }
+
+    public void print(String directory) {
+        System.out.println(directory);
+        try {
+            walk(new File(directory), "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n" + this.dirCount + " directories, " + this.fileCount + " files");
+    }
+
+    private void register(File file) {
+        if (file.isDirectory()) {
+            this.dirCount += 1;
+        } else {
+            this.fileCount += 1;
+        }
+    }
+
+    private static boolean isSymlink(File file) throws IOException {
+        if (file == null)
+            throw new NullPointerException("File must not be null");
+        File canon;
+        if (file.getParent() == null) {
+            canon = file;
+        } else {
+            File canonDir = file.getParentFile().getCanonicalFile();
+            canon = new File(canonDir, file.getName());
+        }
+        return !canon.getCanonicalFile().equals(canon.getAbsoluteFile());
+    }
+
+    private void walk(File folder, String prefix) throws IOException{
+        File file;
+        File[] fileList = folder.listFiles();
+        Arrays.sort(fileList);
+
+        for (int index = 0; index < fileList.length; index++) {
+            file = fileList[index];
+            if (file.getName().charAt(0) == '.') {
+                continue;
+            }
+            register(file);
+
+            if (index == fileList.length - 1) {
+                if (isSymlink(file)){
+                    Path filePath = Paths.get(file.getCanonicalPath());
+                    Path linkPath = Paths.get(file.toPath().toString());
+                    System.out.println(prefix + "└── " + linkPath.relativize(filePath));
+                }else{
+                    System.out.println(prefix + "└── " + file.getName());
+                }
+                if (file.isDirectory()) {
+                    walk(file, prefix + "    ");
+                }
+            } else {
+                if (isSymlink(file)){
+                    Path filePath = Paths.get(file.getCanonicalPath());
+                    Path linkPath = Paths.get(file.toPath().toString());
+                    System.out.println(prefix + "└── " + linkPath.relativize(filePath));
+                }else{
+                    System.out.println(prefix + "└── " + file.getName());
+                }
+                if (file.isDirectory()) {
+                    walk(file, prefix + "│   ");
+                }
+            }
+        }
+    }
+}
---
+++ cf-ops-automation-broker-core/src/test/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/tools/TreeTest.java
@@ -0,0 +1,11 @@
+package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.pipeline.tools;
+
+import org.junit.Test;
+
+public class TreeTest {
+
+    @Test
+    public void test() {
+        (new Tree()).print("/home/losapio/GIT/Coab/paas-templates/coab-depls/mongodb");
+    }
+}

9d0b82615b4454d9cc431539c1bb1cd8819020a6
Jean-Christophe LOSAPIO <jeanchristophe.losapio@orange.com>
2018-06-25 17:59:33 +0200

    Integration testing utilities #2

--- cf-ops-automation-broker-core/src/test/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/tools/TreeTest.java
+++ cf-ops-automation-broker-core/src/test/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/tools/TreeTest.java
@@ -1,10 +1,11 @@
 package com.orange.oss.cloudfoundry.broker.opsautomation.ondemandbroker.pipeline.tools;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TreeTest {
 
-    @Test
+    @Test@Ignore
     public void test() {
         (new Tree()).print("/home/losapio/GIT/Coab/paas-templates/coab-depls/mongodb");
     }

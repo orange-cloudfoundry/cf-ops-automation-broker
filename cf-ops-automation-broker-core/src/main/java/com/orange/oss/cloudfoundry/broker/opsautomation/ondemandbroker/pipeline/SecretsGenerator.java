d4ac294effbff5c1eb6109cf3cfb91ad33b7e071
Jean-Christophe LOSAPIO <jeanchristophe.losapio@orange.com>
2019-10-14 18:04:31 +0200

    Fix https://github.com/orange-cloudfoundry/cf-ops-automation-broker/issues/203

--- cf-ops-automation-broker-core/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/SecretsGenerator.java
+++ cf-ops-automation-broker-core/src/main/java/com/orange/oss/cloudfoundry/broker/opsautomation/ondemandbroker/pipeline/SecretsGenerator.java
@@ -19,15 +19,6 @@ public class SecretsGenerator extends StructureGeneratorImpl implements SecretsR
     public void checkPrerequisites(Path workDir) {
         //Check common pre-requisites
         super.checkPrerequisites(workDir);
-
-        //Check specific pre-requisite (secrets directory)
-        this.checkThatSecretsDirectoryExists(workDir);
-
-        //Check specific pre-requisite (meta file in model secrets directory)
-        this.checkThatMetaFileExists(workDir);
-
-        //Check specific pre-requisite (secrets file in model secrets directory)
-        this.checkThatSecretsFileExists(workDir);
     }
 
     @Override
@@ -35,15 +26,6 @@ public class SecretsGenerator extends StructureGeneratorImpl implements SecretsR
 
         //Generate service directory
         super.generate(workDir, serviceInstanceId, coabVarsFileDto);
-
-        //Generate secrets directory
-        this.generateSecretsDirectory(workDir, serviceInstanceId);
-
-        //Generate meta file
-        this.generateMetaFile(workDir, serviceInstanceId);
-
-        //Generate secrets file
-        this.generateSecretsFile(workDir, serviceInstanceId);
 
         //Generate enable deployment file
         this.generateEnableDeploymentFile(workDir, serviceInstanceId);
@@ -71,86 +53,15 @@ public class SecretsGenerator extends StructureGeneratorImpl implements SecretsR
 
     public void remove(Path workDir, String serviceInstanceId) {
 
-        //Remove meta file
-        this.removeMetaFile(workDir, serviceInstanceId);
-
-        //Remove secrets file
-        this.removeSecretsFile(workDir, serviceInstanceId);
-
         //Remove enable deployment file
         this.removeEnableDeploymentFile(workDir, serviceInstanceId);
 
-    }
-
-    protected void checkThatSecretsDirectoryExists(Path workDir){
-        Path secretsDir = StructureGeneratorHelper.generatePath(workDir,
-                this.rootDeployment,
-                this.modelDeployment,
-                DeploymentConstants.SECRETS);
-        if (StructureGeneratorHelper.isMissingResource(secretsDir)){
-            throw new DeploymentException(DeploymentConstants.SECRETS_EXCEPTION + secretsDir);
-        }
-    }
-
-    protected void checkThatMetaFileExists(Path workDir){
-        Path metaFile = StructureGeneratorHelper.generatePath(workDir,
-                this.rootDeployment,
-                this.modelDeployment,
-                DeploymentConstants.SECRETS,
-                DeploymentConstants.META + DeploymentConstants.YML_EXTENSION);
-        if (StructureGeneratorHelper.isMissingResource(metaFile)){
-            throw new DeploymentException(DeploymentConstants.META_FILE_EXCEPTION);
-        }
-    }
-
-    protected void checkThatSecretsFileExists(Path workDir){
-        Path secretsFile = StructureGeneratorHelper.generatePath(workDir,
-                this.rootDeployment,
-                this.modelDeployment,
-                DeploymentConstants.SECRETS,
-                DeploymentConstants.SECRETS + DeploymentConstants.YML_EXTENSION);
-        if (StructureGeneratorHelper.isMissingResource(secretsFile)){
-            throw new DeploymentException(DeploymentConstants.SECRETS_FILE_EXCEPTION);
-        }
-    }
-
-    protected void generateSecretsDirectory(Path workDir, String serviceInstanceId){
-        StructureGeneratorHelper.generateDirectory(workDir,
-                this.rootDeployment,
-                this.computeDeploymentName(serviceInstanceId),
-                DeploymentConstants.SECRETS);
-    }
-
-    protected void generateMetaFile(Path workDir, String serviceInstanceId){
-        String[] sourcePathElements = new String[] {this.rootDeployment, this.modelDeployment, DeploymentConstants.SECRETS};
-        String[] targetPathElements = new String[] {this.rootDeployment, this.computeDeploymentName(serviceInstanceId), DeploymentConstants.SECRETS};
-        String fileName = DeploymentConstants.META + DeploymentConstants.YML_EXTENSION;
-        StructureGeneratorHelper.generateSymbolicLink(workDir, sourcePathElements, targetPathElements, fileName, fileName);
-    }
-
-    protected void generateSecretsFile(Path workDir, String serviceInstanceId){
-        String[] sourcePathElements = new String[] {this.rootDeployment, this.modelDeployment, DeploymentConstants.SECRETS};
-        String[] targetPathElements = new String[] {this.rootDeployment, this.computeDeploymentName(serviceInstanceId), DeploymentConstants.SECRETS};
-        String fileName = DeploymentConstants.SECRETS + DeploymentConstants.YML_EXTENSION;
-        StructureGeneratorHelper.generateSymbolicLink(workDir, sourcePathElements, targetPathElements, fileName, fileName);
     }
 
     protected void generateEnableDeploymentFile(Path workDir, String serviceInstanceId){
         String[] targetPathElements = new String[] {this.rootDeployment, this.computeDeploymentName(serviceInstanceId)};
         String fileName = DeploymentConstants.ENABLE_DEPLOYMENT_FILENAME;
         StructureGeneratorHelper.generateFile(workDir,  targetPathElements, fileName, fileName, null);
-    }
-
-    protected void removeMetaFile(Path workDir, String serviceInstanceId) {
-        String[] pathElements = new String[] {this.rootDeployment, this.computeDeploymentName(serviceInstanceId), DeploymentConstants.SECRETS};
-        String fileName = DeploymentConstants.META + DeploymentConstants.YML_EXTENSION;
-        StructureGeneratorHelper.removeFile(workDir, pathElements, fileName);
-    }
-
-    protected void removeSecretsFile(Path workDir, String serviceInstanceId) {
-        String[] pathElements = new String[] {this.rootDeployment, this.computeDeploymentName(serviceInstanceId), DeploymentConstants.SECRETS};
-        String fileName = DeploymentConstants.SECRETS + DeploymentConstants.YML_EXTENSION;
-        StructureGeneratorHelper.removeFile(workDir, pathElements, fileName);
     }
 
     protected void removeEnableDeploymentFile(Path workDir, String serviceInstanceId) {
